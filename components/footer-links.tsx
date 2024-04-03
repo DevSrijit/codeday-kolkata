61356be2e27ac48db89a5cdc459a90677e76336a
devsrijit <srijitghosh7331@gmail.com>
2024-04-03 14:59:15 +0530

    Ship 2 !

--- components/footer-links.tsx
+++ components/footer-links.tsx
@@ -1,39 +1,47 @@
 import {
   SiInstagram as Instagram,
-  SiDiscord as Discord,
   SiGithub as Github,
-  SiTwitter as Twitter
+  SiThreads as Threads,
+  SiLinkedin as Linkedin,
+  SiX as X,
 } from '@icons-pack/react-simple-icons';
 
 const FooterLinks = () => (
   <div className="flex flex-row justify-center gap-x-8">
     <a
-      href="https://github.com/purduehackers"
+      href="https://github.com/CodeDay-Kolkata"
       className="transition hover:scale-110"
       target="_blank"
     >
       <Github height={32} width={32} />
     </a>
     <a
-      href="https://puhack.horse/discord"
+      href="https://www.threads.net/@codeday_kolkata"
       className="transition hover:scale-110 hover:text-discord-vibrant"
       target="_blank"
     >
-      <Discord height={32} width={32} />
+      <Threads height={32} width={32} />
     </a>
     <a
-      href="https://instagram.com/purduehackers"
+      href="https://www.instagram.com/codeday_kolkata"
       className="transition hover:scale-110 hover:text-[#c32aa3]"
       target="_blank"
     >
       <Instagram height={32} width={32} />
     </a>
     <a
-      href="https://twitter.com/purduehackers"
+      href="https://twitter.com/CodeDayKolkata"
       className="transition hover:scale-110 hover:text-[#1DA1F2]"
       target="_blank"
     >
-      <Twitter height={32} width={32} />
+      <X height={32} width={32} />
+    </a>
+    <a
+      href="https://www.linkedin.com/company/codeday-kolkata/"
+      className="transition hover:scale-110 hover:text-[#1DA1F2]"
+      target="_blank"
+    >
+      <Linkedin height={32} width={32} />
     </a>
   </div>
 );
