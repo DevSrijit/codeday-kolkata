804a3bf57d4fd1971d4dbc513e8dd1b843771f92
devsrijit <srijitghosh7331@gmail.com>
2024-04-22 18:54:10 +0530

    Minor Fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,32 +7,8 @@ export default function Document() {
         <meta name="title" content="CodeDay Kolkata" />
         <meta
           name="description"
-          content="CodeDay Kolkata is a vibrant, inclusive gathering where students of all backgrounds explore the joy of coding through creativity and collaboration."
+          content="A 24 hours coding event where students collaborate, learn, and build kick-ass technical projects 💛⚡️"
         />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://cdkol.live/" />
-        <meta property="og:title" content="CodeDay Kolkata" />
-        <meta
-          property="og:description"
-          content="CodeDay Kolkata is a vibrant, inclusive gathering where students of all backgrounds explore the joy of coding through creativity and collaboration. Participants, regardless of coding experience, come together to build games and apps in a supportive environment. With workshops, projects, and a focus on fun, CodeDay inspires a new generation of tech enthusiasts. Our events have had on over 60,000 student alumni through 515 events conducted worldwide. 💫 💻"
-        />
-        <meta
-          property="og:image"
-          content="https://cdkol.live/img/cdk-logo.png"
-        />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://cdkol.live/" />
-        <meta property="twitter:title" content="CodeDay Kolkata" />
-        <meta
-          property="twitter:description"
-          content="CodeDay Kolkata is a vibrant, inclusive gathering where students of all backgrounds explore the joy of coding through creativity and collaboration. Participants, regardless of coding experience, come together to build games and apps in a supportive environment. With workshops, projects, and a focus on fun, CodeDay inspires a new generation of tech enthusiasts. Our events have had on over 60,000 student alumni through 515 events conducted worldwide. 💫 💻"
-        />
-        <meta
-          property="twitter:image"
-          content="https://cdkol.live/img/cdk-logo.png"
-        />
-
         <meta
           name="keywords"
           content="codeday, codeday kolkata, code kolkata, codeday in kolkata, kolkata coding, coding in kolkata, coding tutorial in kolkata, events in kolkata, hackathon, teenager coding, coding, hackathon in kolkata, kolkata hackathon, teenager event in kolkata, teenager hackathon, coding for teenagers, high school event, high school coding, high school hackathon, hackathon for high schoolers, coding event for high schoolers"
