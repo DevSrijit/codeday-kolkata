337bbbe5c14a5db42deefb3b21fba536524fa02c
devsrijit <srijitghosh7331@gmail.com>
2024-07-10 00:44:14 +0530

    chore: Add Clarity analytics script to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,6 +38,18 @@ export default function Document() {
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="English" />
         <meta name="revisit-after" content="2 days" />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "n4sulhztsn");
+            `
+          }}
+        />
       </Head>
       <body>
         <Main />
